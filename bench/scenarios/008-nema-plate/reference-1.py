8a0d6d2ca64351f0c1827391ecb7c32e19ae2a1c
Mikhail Angelov <mikhail.angelov@gmail.com>
2026-07-27 21:27:13 +0300

    fix bench

--- bench/scenarios/008-nema-plate/reference-1.py
+++ bench/scenarios/008-nema-plate/reference-1.py
@@ -5,6 +5,7 @@ result = (
     .box(45, 45, 5, centered=(True, True, False))
     .faces(">Z").workplane()
     .hole(22)                                     # central bore
+    .faces(">Z").workplane()
     .rect(31, 31, forConstruction=True).vertices()
     .hole(3.4)                                    # four mounting holes
 )
